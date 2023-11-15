2678ec200a3f97ce6e5f003dfb224ce82d8002ba
Detrick Lewis <Detricklew@gmail.com>
2023-11-15 15:03:22 -0600

    finish project

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 http.createServer(function ( req, res ) {
-    console.log(req.url);
-    let content;
-    if(req.url.toLowerCase() === '/about'){
-        console.log('hey');
-        content = 'hey you are on the about me page'
+    const q = url.parse(req.url, true);
+    console.log(q.pathname);
+    if (q.pathname == '/') q.pathname = '/index';
+    if (q.pathname == '/favicon.ico') return;
+    const filename = `./templates${q.pathname}.html`;
+    try {
+        const data = fs.readFileSync(filename);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    } catch (err) {
+        fs.readFile('./templates/404.html', (err, data) => {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                return res.end("404 Not Found");
+              } 
+              console.log(data);
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.write(data);
+                return res.end();
+        })
     }
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(content);
-    res.end();
+
 }).listen(8080);
